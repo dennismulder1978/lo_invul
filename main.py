d824a8e6f5de1a87bbd8d018af5e3535fe7af44e
Dennis Werk <dnns.mldr@gmail.com>
2024-06-11 12:42:35 +0200

    progress in frames

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MyApp(tk.Tk):
         super().__init__()
         
         # variables
-        self.pad = 5
+        self.pad = 2
         
         # initialization
         self.title('Automatisch invullen van lichamelijk onderzoek in HIS')
@@ -33,32 +33,52 @@ class MyApp(tk.Tk):
         self.config(menu=menubar)
         
     def create_main_frame(self):
-        frame = tk.Frame(self, bg='blue')
-        frame.pack(fill='both', expand=True, padx=self.pad, pady=self.pad)
+        main_frame = tk.Frame(self, bg='yellow')
+        main_frame.grid(row=0, column=0)
         
-        for keyword in data.physical.keys():
-            # New paragraph
-            sub_frame = tk.Frame(frame, bg="lightblue")
-            sub_frame.pack(fill='both', expand=True, padx=self.pad, pady=self.pad)
-            
-            # Title of paragraph in its own frame
-            label_frame = tk.Frame(sub_frame, height=1)
-            label_frame.pack()
-            name = tk.Label(sub_frame, textvariable=tk.StringVar(sub_frame,keyword), anchor='nw')
-            name.pack(fill='x')
-            
-            # input of the paragraph
-            input_frame = tk.Frame(sub_frame)
-            input_frame.pack(side='top', fill='both', expand=True)
-            for each_item in data.physical[keyword]:
-                item_label = tk.Label(input_frame, textvariable=tk.StringVar(input_frame, each_item), anchor='nw')
-                item_label.pack(fill='both')
+        # titel
+        title_frame = tk.Frame(main_frame, bg="red", borderwidth=1, relief="solid").grid(row=0, column=0, padx=5, pady=5)
+        tk.Label(title_frame, textvariable=tk.StringVar(title_frame,"Meetgegevens")).grid(row=0, column=0, sticky='w')
+        
+        # input_frame of "Meetgegevens"
+        input_frame = tk.Frame(main_frame, bg="yellow", borderwidth=1, relief="solid").grid(row=1, column=0, sticky="news")
+        
+        # item to input
+        column_number = 0
+        row_number = 1
+        width_number = 5
+        
+        item_frame = tk.Frame(input_frame, bg="green", borderwidth=1, relief="solid").grid(row=row_number, column=column_number, sticky="news")
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Lengte"), bg="yellow", anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        lengte_entry = tk.Entry(item_frame, justify='left', width=width_number).grid(row=row_number+1, column=column_number, padx=5, sticky='ww')
+        
+        column_number += 1
+        item_frame = tk.Frame(input_frame, bg="green", borderwidth=1, relief="solid").grid(row=row_number, column=column_number)
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Gewicht"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        gewicht_entry = tk.Entry(item_frame, justify='left', width=width_number).grid(row=row_number+1, column=column_number, padx=5, sticky='w')
+        
+        # column_number += 1
+        # item_frame = tk.Frame(input_frame).grid(row=row_number, column=column_number)
+        # tk.Label(frame, textvariable=tk.StringVar(frame, "Systolische bloeddruk"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        # syst_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
+        # column_number += 2
+        
+        # tk.Label(frame, textvariable=tk.StringVar(frame, "Diastolische bloeddruk"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        # diast_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
+        # column_number += 2
+        
+        # tk.Label(frame, textvariable=tk.StringVar(frame, "Pols"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        # pols_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
+        # column_number += 2
+        
+        # tk.Label(frame, textvariable=tk.StringVar(frame, "Saturatie %"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        # sat_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
+        # column_number += 2
+
         
         # add a submit button
-        button_frame = tk.Frame(frame, bg='lightblue')
-        button_frame.pack(fill='both')
-        button = tk.Button(button_frame, text='Kopieer naar HIS', width=20, height=3, command=self.quit)
-        button.pack(padx=self.pad, pady=self.pad, anchor='se')
+        button_frame = tk.Frame(main_frame).grid(row=100, column=0, pady=10)
+        button = tk.Button(button_frame, text='Kopieer naar HIS', width=20, height=3, command=self.quit).grid(row=3, column=0, padx=5, pady=5)
     
             
     def button_click(self):
