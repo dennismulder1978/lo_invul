811b1a7901c9530ee2daa83cc11e715f30ea5ddb
Dennis Werk <dnns.mldr@gmail.com>
2024-06-11 14:33:30 +0200

    first draft

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 physical = {
-    'Zuigeling': ["Helder en alert", "Ptechien", "Nekstijf", "Fontanel in niveau", "ADS normaal"],
-    'Locomotorius': [],
-    'Romp':[],
-    'Neurologie': [],
+    'Zuigeling': "Niet acuut zieke zuigeling, helder en alert.\nNiet nekstijf en geen ptechien\nFontanel in niveau, natte slijmvliezen\nADS rustig, geen rhinorroe, keel rustig.\nPulm nag, b-, cor s1s2 $, abd:np, wt, dp-",
+    'Locomotorius': "Nek: geen beperkingen actieve en passieve bewegingen, Lhermitte neg.\nSchouder li/re: abd-, rot-, AC-\nGeen druk-/kloppijn WK, vrije bewegingen, Schober: normaal, lasegue neg\nHeup li/re geen pijn rotatie, geen asdrukpijn, geen drukpijn bursa\nKnie li/re geen hdyrops, McMurray neg, stabiel\nEnkels/Voeten gda.",
+    'Hartlong':"Pulm nag, b-, geen opwekbare thoraxpijn\nCor s1s2 reg $\nGeen verhoogde CVD, geen oedemen.",
+    'Neurologie': "HH-zenuwen symm niet afw.\nSens intakt, motoriek intakt\nBPR/KPR/APR/VZR niet afwijkend\nRhomberg ongestoord, geen dysdiadochokinese",
     'Oog': [],
 }
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MyApp(tk.Tk):
         
         # initialization
         self.title('Automatisch invullen van lichamelijk onderzoek in HIS')
-        self.minsize(800,400)
+        self.minsize(700,600)
                 
         # adding items
         self.create_menu()
@@ -33,110 +33,147 @@ class MyApp(tk.Tk):
         self.config(menu=menubar)
         
     def create_main_frame(self):
-        main_frame = tk.Frame(self, bg='yellow')
+        # radio options
+        self.selected_option = tk.IntVar()
+        options = {
+            "self.zuigeling_radio": "zuigeling_radio",
+            "self.locomotorius_radio": "locomotorius_radio",
+        }
+        
+        # Start frames
+        main_frame = tk.Frame(self)
         main_frame.grid(row=0, column=0)
         
-        # titel
-        title_frame = tk.Frame(main_frame, bg="red", borderwidth=1, relief="solid").grid(row=0, column=0, padx=5, pady=5)
-        tk.Label(title_frame, textvariable=tk.StringVar(title_frame,"Meetgegevens")).grid(row=0, column=0, sticky='w')
+        # add a submit button
+        new_frame = tk.Frame(main_frame, bg="blue", borderwidth=1, relief="solid").grid(row=0, column=0, padx= 5, pady=5)
+        new_button = tk.Button(new_frame, text='Quit', width=15, height=2, command=self.quit)
+        new_button.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='w')
         
-        # input_frame of "Meetgegevens"
-        input_frame = tk.Frame(main_frame, bg="yellow", borderwidth=1, relief="solid").grid(row=1, column=0, sticky="news")
+        
+        # input_frame
+        input_frame = tk.Frame(main_frame, bg="yellow", borderwidth=1, relief="solid").grid(row=1, column=0, pady=5)
+        tk.Label(input_frame, textvariable=tk.StringVar(input_frame,"Meetgegevens")).grid(row=1, column=0, columnspan=2, sticky='w')
         
         # item to input
-        column_number = 0
-        row_number = 1
+        column_number = 1
+        row_number = 2
         width_number = 5
+        item_width = 50
         
-        item_frame = tk.Frame(input_frame, bg="green", borderwidth=1, relief="solid").grid(row=row_number, column=column_number, sticky="news")
-        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Lengte"), bg="yellow", anchor='nw').grid(row=row_number, column=column_number, sticky='w')
-        lengte_entry = tk.Entry(item_frame, justify='left', width=width_number).grid(row=row_number+1, column=column_number, padx=5, sticky='ww')
-        
+        item_frame = tk.Frame(input_frame, width=item_width).grid(row=row_number, column=column_number)
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Systolische bloeddruk"), anchor='nw').grid(row=row_number, column=column_number, sticky='w', padx=50)
+        self.syst_entry = tk.Entry(item_frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='ew')
         column_number += 1
-        item_frame = tk.Frame(input_frame, bg="green", borderwidth=1, relief="solid").grid(row=row_number, column=column_number)
+        
+        item_frame = tk.Frame(input_frame, width=item_width).grid(row=row_number, column=column_number)
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Diastolische bloeddruk"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        self.diast_entry = tk.Entry(item_frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='ew')
+        column_number += 1
+        
+        item_frame = tk.Frame(input_frame, width=item_width).grid(row=row_number, column=column_number)
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Lengte"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        self.lengte_entry = tk.Entry(item_frame, justify='left', width=width_number).grid(row=row_number+1, column=column_number, padx=5, sticky='ew')
+        column_number += 1
+        
+        item_frame = tk.Frame(input_frame, width=item_width).grid(row=row_number, column=column_number)
         tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Gewicht"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
-        gewicht_entry = tk.Entry(item_frame, justify='left', width=width_number).grid(row=row_number+1, column=column_number, padx=5, sticky='w')
+        self.gewicht_entry = tk.Entry(item_frame, justify='left', width=width_number).grid(row=row_number+1, column=column_number, padx=5, sticky='ew')
+        column_number += 1
         
-        # column_number += 1
-        # item_frame = tk.Frame(input_frame).grid(row=row_number, column=column_number)
-        # tk.Label(frame, textvariable=tk.StringVar(frame, "Systolische bloeddruk"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
-        # syst_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
-        # column_number += 2
+        item_frame = tk.Frame(input_frame, width=item_width).grid(row=row_number, column=column_number)
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Pols"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        self.pols_entry = tk.Entry(item_frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='ew')
+        column_number += 1
         
-        # tk.Label(frame, textvariable=tk.StringVar(frame, "Diastolische bloeddruk"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
-        # diast_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
-        # column_number += 2
-        
-        # tk.Label(frame, textvariable=tk.StringVar(frame, "Pols"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
-        # pols_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
-        # column_number += 2
-        
-        # tk.Label(frame, textvariable=tk.StringVar(frame, "Saturatie %"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
-        # sat_entry = tk.Entry(frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='w')
-        # column_number += 2
+        item_frame = tk.Frame(input_frame, width=item_width).grid(row=row_number, column=column_number)
+        tk.Label(item_frame, textvariable=tk.StringVar(item_frame, "Saturatie %"), anchor='nw').grid(row=row_number, column=column_number, sticky='w')
+        self.sat_entry = tk.Entry(item_frame, justify='left', width=4).grid(row=row_number+1, column=column_number, padx=self.pad, sticky='ew')
+        column_number += 1
 
+        for category in data.physical.keys():
+            print(category)
+        # kinder
+        self.zuigeling_radio = tk.Radiobutton(input_frame, text="Zuigeling onderzoek", variable=self.selected_option, value=1)
+        self.zuigeling_radio.grid(row=5, column=0, columnspan=2, sticky='w')
+        self.zuigeling_entry = tk.Text(input_frame, width=80, height=8)
+        self.zuigeling_entry.grid(row=6, column=1, columnspan=6, padx=self.pad, sticky='ew')
+        self.zuigeling_entry.insert("1.0", data.physical['Zuigeling'])
         
-        # add a submit button
-        button_frame = tk.Frame(main_frame).grid(row=100, column=0, pady=10)
-        button = tk.Button(button_frame, text='Kopieer naar HIS', width=20, height=3, command=self.quit).grid(row=3, column=0, padx=5, pady=5)
-    
+        # loco motorius
+        self.locomotorius_radio = tk.Radiobutton(input_frame, text="Locomotorius onderzoek", variable=self.selected_option, value=2)
+        self.locomotorius_radio.grid(row=7, column=0, columnspan=2, sticky='w')
+        self.locomotorius_entry = tk.Text(input_frame, width=80, height=8)
+        self.locomotorius_entry.grid(row=8, column=1, columnspan=6, padx=self.pad, sticky='ew')
+        self.locomotorius_entry.insert("1.0", data.physical['Locomotorius'])
             
-    def button_click(self):
-        with pyautogui.hold("alt"):
-            pyautogui.press("tab")
-        # pyautogui.getWindowsWithTitle("Medicom*")[0]
-        sleep(2)
-        pyautogui.hotkey("shift", "f12")
-        sleep(2)
-        pyautogui.typewrite("Voorbeeldtekst regel S", interval=0.01)
-        for i in range (3):
-            pyautogui.press("tab")
-        pyautogui.typewrite("C")
-        pyautogui.press("tab")
-        pyautogui.typewrite("Voorbeeldtekst regel O", interval=0.01)
-        for i in range (4):
-            pyautogui.press("tab")
-        pyautogui.typewrite("L01")
-        pyautogui.press("enter")
-        sleep(0.5)
-        with pyautogui.hold("alt"):
-            pyautogui.press("s")
-        sleep(0.5)
-        for i in range (4):
-            pyautogui.press("tab")
-        pyautogui.typewrite("Voorbeeldtekst regel P", interval=0.01)
-        with pyautogui.hold("shift"):
-            pyautogui.press("f6")
-        sleep(2)
-        for i in range (4):
-            pyautogui.press("tab")
-        pyautogui.typewrite("GEWICHT", interval=0.01)
-        sleep(0.4)
-        pyautogui.press("enter")
-        sleep(0.1)
-        pyautogui.press("enter")
-        sleep(0.2)
-        pyautogui.typewrite("76,5")
-        sleep(0.5)
-        pyautogui.press("enter")
-        sleep(1.0)
-        with pyautogui.hold("shift"):
-            pyautogui.press("f6")
-        sleep(2)
-        for i in range (4):
-            pyautogui.press("tab")
-        pyautogui.typewrite("TEMPERATUU", interval=0.01)
-        sleep(0.4)
-        pyautogui.press("enter")
-        sleep(0.1)
-        pyautogui.press("enter")
-        sleep(0.2)
-        pyautogui.typewrite("38,1")
-        sleep(0.5)
-        pyautogui.press("enter")
+        # add a submit button
+        button_frame = tk.Frame(main_frame, bg="blue", borderwidth=1, relief="solid").grid(row=100, column=0, padx= 5, pady=5)
+        button = tk.Button(button_frame, text='Kopieer naar HIS', width=15, height=2, command=self.button_click)
+        button.grid(row=100, column=0, columnspan=2, padx=5, pady=5, sticky='w')
         
-        with pyautogui.hold("alt"):
-            pyautogui.press("s")
+    def button_click(self):
+        print(self.selected_option.get())
+        text = ""
+        if self.selected_option.get() == 1:
+            text = self.zuigeling_entry.get("1.0", tk.END)
+        elif self.selected_option.get() == 2:
+            text = self.locomotorius_entry.get("1.0", tk.END)
+        print(text)
+        # with pyautogui.hold("alt"):
+        #     pyautogui.press("tab")
+        # # pyautogui.getWindowsWithTitle("Medicom*")[0]
+        # sleep(2)
+        # pyautogui.hotkey("shift", "f12")
+        # sleep(2)
+        # pyautogui.typewrite("Voorbeeldtekst regel S", interval=0.01)
+        # for i in range (3):
+        #     pyautogui.press("tab")
+        # pyautogui.typewrite("C")
+        # pyautogui.press("tab")
+        # pyautogui.typewrite("Voorbeeldtekst regel O", interval=0.01)
+        # for i in range (4):
+        #     pyautogui.press("tab")
+        # pyautogui.typewrite("L01")
+        # pyautogui.press("enter")
+        # sleep(0.5)
+        # with pyautogui.hold("alt"):
+        #     pyautogui.press("s")
+        # sleep(0.5)
+        # for i in range (4):
+        #     pyautogui.press("tab")
+        # pyautogui.typewrite("Voorbeeldtekst regel P", interval=0.01)
+        # with pyautogui.hold("shift"):
+        #     pyautogui.press("f6")
+        # sleep(2)
+        # for i in range (4):
+        #     pyautogui.press("tab")
+        # pyautogui.typewrite("GEWICHT", interval=0.01)
+        # sleep(0.4)
+        # pyautogui.press("enter")
+        # sleep(0.1)
+        # pyautogui.press("enter")
+        # sleep(0.2)
+        # pyautogui.typewrite("76,5")
+        # sleep(0.5)
+        # pyautogui.press("enter")
+        # sleep(1.0)
+        # with pyautogui.hold("shift"):
+        #     pyautogui.press("f6")
+        # sleep(2)
+        # for i in range (4):
+        #     pyautogui.press("tab")
+        # pyautogui.typewrite("TEMPERATUU", interval=0.01)
+        # sleep(0.4)
+        # pyautogui.press("enter")
+        # sleep(0.1)
+        # pyautogui.press("enter")
+        # sleep(0.2)
+        # pyautogui.typewrite("38,1")
+        # sleep(0.5)
+        # pyautogui.press("enter")
+        
+        # with pyautogui.hold("alt"):
+        #     pyautogui.press("s")
 
 
 
